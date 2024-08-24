651b46201c174ecced849be10ed67d9dc710735e
Choollol <chrisun8@yahoo.com>
2024-08-24 15:55:47 -0700

    properties setup

---
+++ src/main/java/net/choollol/test_mod/TM.java
@@ -0,0 +1,8 @@
+package net.choollol.test_mod;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(TM.ID)
+public class TM {
+    public static final String ID = "test_mod";
+}
---
+++ src/main/java/net/choollol/test_mod/TMBlocks.java
@@ -0,0 +1,7 @@
+package net.choollol.test_mod;
+
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class TMBlocks {
+    public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(TM.ID);
+}
